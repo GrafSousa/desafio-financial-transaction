efb1a065313e3c3b142932dc02bc21f8fa3a2893
Gustavo <graf.sousa@gmail.com>
2020-04-27 15:20:27 -0300

    [feature]: Add retrieve/create transaction

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,30 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.sumTransactionValueByType('income');
+    const outcome = this.sumTransactionValueByType('outcome');
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  private sumTransactionValueByType(type: string): number {
+    return this.transactions
+      .filter(transaction => transaction.type === `${type}`)
+      .reduce((sum, transaction) => sum + transaction.value, 0);
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
